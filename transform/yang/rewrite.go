6bc025bac6b201a265b748f73aded4cf164edfff
Sho SHIMIZU <sho.shimizu@gmail.com>
2017-06-13 14:31:56 -0700

    Insert zero value in Enum if no zero value there

--- transform/yang/rewrite.go
+++ transform/yang/rewrite.go
@@ -1,6 +1,8 @@
 package yang
 
 import (
+	"strings"
+
 	"github.com/oshothebig/pbast"
 )
 
@@ -36,4 +38,61 @@ func RemoveRootMessage(f *pbast.File) *pbast.File {
 	ret := *f
 	ret.Messages = ms
 	return &ret
+}
+
+func CompleteZeroInEnum(f *pbast.File) *pbast.File {
+	if len(f.Messages) == 0 && len(f.Enums) == 0 {
+		return f
+	}
+
+	newFile := *f
+	var enums []*pbast.Enum
+	for _, e := range f.Enums {
+		enums = append(enums, completeZeroIfAbsent(e))
+	}
+	newFile.Enums = enums
+
+	var messages []*pbast.Message
+	for _, m := range f.Messages {
+		messages = append(messages, completeZeroInMessage(m))
+	}
+	newFile.Messages = messages
+
+	return &newFile
+}
+
+func completeZeroInMessage(m *pbast.Message) *pbast.Message {
+	if len(m.Messages) == 0 && len(m.Enums) == 0 {
+		return m
+	}
+
+	newMessage := *m
+	var enums []*pbast.Enum
+	for _, e := range m.Enums {
+		enums = append(enums, completeZeroIfAbsent(e))
+	}
+	newMessage.Enums = enums
+
+	var messages []*pbast.Message
+	for _, m := range m.Messages {
+		messages = append(messages, completeZeroInMessage(m))
+	}
+	newMessage.Messages = messages
+
+	return &newMessage
+}
+
+func completeZeroIfAbsent(e *pbast.Enum) *pbast.Enum {
+	const suffix = "_DEFAULT"
+	for _, v := range e.Fields {
+		// there is 0, no need to complete
+		if v.Index == 0 {
+			return e
+		}
+	}
+
+	field := pbast.NewEnumField(strings.ToUpper(e.Name)+suffix, 0)
+	newEnum := *e
+	newEnum.Fields = append([]*pbast.EnumField{field}, newEnum.Fields...)
+	return &newEnum
 }
--- transform/yang/rewrite_test.go
+++ transform/yang/rewrite_test.go
@@ -63,3 +63,112 @@ func TestRemoveRootMessage(t *testing.T) {
 		}
 	}
 }
+
+func TestCompleteZeroInEnum(t *testing.T) {
+	table := []struct {
+		in       *pbast.File
+		expected *pbast.File
+	}{
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 1))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("E1_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V1", 1))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 0))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 0))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 1))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 1))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("E1_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V1", 1))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("E2_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V2", 1))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 1))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("E2_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V2", 1))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 1))).
+				AddMessage(pbast.NewMessage("M1").
+					AddEnum(pbast.NewEnum("E1").
+						AddField(pbast.NewEnumField("V1", 0)))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("E2_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V2", 1))).
+				AddMessage(pbast.NewMessage("M1").
+					AddEnum(pbast.NewEnum("E1").
+						AddField(pbast.NewEnumField("V1", 0)))),
+		},
+		{
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("V2", 1))).
+				AddMessage(pbast.NewMessage("M1").
+					AddEnum(pbast.NewEnum("E1").
+						AddField(pbast.NewEnumField("V1", 1)))),
+			pbast.NewFile("org.example").
+				AddEnum(pbast.NewEnum("E1").
+					AddField(pbast.NewEnumField("V1", 0))).
+				AddEnum(pbast.NewEnum("E2").
+					AddField(pbast.NewEnumField("E2_DEFAULT", 0)).
+					AddField(pbast.NewEnumField("V2", 1))).
+				AddMessage(pbast.NewMessage("M1").
+					AddEnum(pbast.NewEnum("E1").
+						AddField(pbast.NewEnumField("E1_DEFAULT", 0)).
+						AddField(pbast.NewEnumField("V1", 1)))),
+		},
+	}
+
+	for x, d := range table {
+		if actual := CompleteZeroInEnum(d.in); !reflect.DeepEqual(actual, d.expected) {
+			t.Errorf("#%d: got %v, want %v", x, actual, d.expected)
+		}
+	}
+}
