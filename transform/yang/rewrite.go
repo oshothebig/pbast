1e3f3b083fd6d36aedd0367a454b435ed3549cef
Sho SHIMIZU <sho.shimizu@gmail.com>
2017-09-01 15:55:34 -0700

    Remove unused AST transform function

--- transform/yang/rewrite.go
+++ transform/yang/rewrite.go
@@ -6,68 +6,6 @@ import (
 	"github.com/oshothebig/pbast"
 )
 
-func RemoveRootMessage(f *pbast.File) *pbast.File {
-	if len(f.Messages) == 0 {
-		return f
-	}
-
-	index, root := rootMessage(f)
-	if root == nil {
-		return f
-	}
-
-	messages := make([]*pbast.Message, 0, len(f.Messages)-1)
-	messages = append(messages, f.Messages[:index]...)
-	messages = append(messages, f.Messages[index+1:]...)
-
-	names := newStringSet()
-	for _, x := range messages {
-		names.add(x.Name)
-	}
-	for _, x := range f.Enums {
-		names.add(x.Name)
-	}
-	for _, x := range f.Services {
-		names.add(x.Name)
-	}
-
-	for _, m := range root.Messages {
-		// naming overlap is found, then root message cann't be removed
-		if names.contains(m.Name) {
-			return f
-		}
-	}
-
-	if len(root.Fields) != 0 {
-		return f
-	}
-
-	ret := *f
-	if len(root.Messages) == 0 {
-		ret.Messages = messages
-		return &ret
-	}
-
-	ret.Enums = append(ret.Enums, root.Enums...)
-	newMessages := make([]*pbast.Message, 0, len(f.Messages)+len(root.Messages)-1)
-	newMessages = append(newMessages, f.Messages[:index]...)
-	newMessages = append(newMessages, root.Messages...)
-	newMessages = append(newMessages, f.Messages[index+1:]...)
-	ret.Messages = newMessages
-
-	return &ret
-}
-
-func rootMessage(f *pbast.File) (int, *pbast.Message) {
-	for i, m := range f.Messages {
-		if m.Name == "Root" {
-			return i, m
-		}
-	}
-
-	return -1, nil
-}
-
 func CompleteZeroInEnum(f *pbast.File) *pbast.File {
 	if len(f.Messages) == 0 && len(f.Enums) == 0 {
 		return f
