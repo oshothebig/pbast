b1282a673d0e0c7767b2de34903f5f14c204ea53
Sho SHIMIZU <sho.shimizu@gmail.com>
2017-09-20 18:38:40 -0700

    Make return type stream for notification conversion

--- transform/yang/transform.go
+++ transform/yang/transform.go
@@ -171,7 +171,9 @@ func (t *transformer) notification(e entry) *pbast.RPC {
 	out := t.buildMessage(buildName(method, common, "Response"), e)
 	t.declare(out)
 
-	return pbast.NewRPC(method, pbast.NewReturnType(in.TypeName()), pbast.NewReturnType(out.TypeName()))
+	returnType := pbast.NewReturnType(out.TypeName())
+	returnType.Streamable = true
+	return pbast.NewRPC(method, pbast.NewReturnType(in.TypeName()), returnType)
 }
 
 func (t *transformer) buildMessage(name string, e entry) *pbast.Message {
